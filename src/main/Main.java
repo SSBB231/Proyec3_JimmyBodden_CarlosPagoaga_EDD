6429beebaabb341561af42fa3ebc2c9eceb5a9b7
11241069 <carlos.pagoaga@unitec.edu>
2014-03-06 09:56:54 -0600

    Aki te vah con el nombre

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,8 +7,18 @@
 package main;
 
 import MyClasses.MyLink;
+import edu.uci.ics.jung.algorithms.layout.AggregateLayout;
+
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.FRLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Paint;
 import java.io.BufferedReader;
 
 import java.io.File;
@@ -16,7 +26,10 @@ import java.io.File;
 import java.io.FileReader;
 
 import java.util.ArrayList;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import org.apache.commons.collections15.Transformer;
+
 
 /**
  *
@@ -61,6 +74,27 @@ public class Main
         }else{
                 JOptionPane.showMessageDialog(null, "Error No Existe el archivo");
             }
+            
+ // The Layout<V, E> is parameterized by the vertex and edge types
+ Layout<String,MyLink> layout = new FRLayout(mainGraph);
+ layout.setSize(new Dimension(700,700)); // sets the initial size of the space
+ // The BasicVisualizationServer<V,E> is parameterized by the edge types
+ BasicVisualizationServer<String,MyLink> vv =  new BasicVisualizationServer<String,MyLink>(layout);
+ vv.setPreferredSize(new Dimension(800,800)); //Sets the viewing area size
+ Transformer<String,Paint> trans = new Transformer<String,Paint>(){
+          public Paint transform(String i){
+              return Color.CYAN;
+          }
+      };
+ 
+vv.getRenderContext().setVertexFillPaintTransformer(trans);
+vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+ JFrame frame = new JFrame("Simple Graph View");
+ frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ frame.getContentPane().add(vv); 
+ frame.pack();
+ frame.setVisible(true); 
+
             //Aquí se lee el archivo de texto para poder agregar todas las aristas al grafo. 
             //Esto crea, a su vez, los vértices del grafo.----------------------------------------------------------------------------------------
             
@@ -73,6 +107,6 @@ public class Main
             //mainGraph.addEdge(new MyLink(string1, string2, weight), string1, string2);
             
             //----------------------------------------------------------------------------------------------------------------------------------------------------
-      //mira hay una cosa que no estoy seguro lo que es la aereolinea no lo ocupamos o que siempre esta en el archivo de texto
+            //mira hay una cosa que no estoy seguro lo que es la aereolinea no lo ocupamos o que siempre esta en el archivo de texto
       }
 }
